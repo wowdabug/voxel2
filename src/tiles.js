905e6243bfa35af1ba4015ddbe474b980ef04f7d
wowdabug <110950607+wowdabug@users.noreply.github.com>
2026-07-10 12:19:41 -0500

    wip tile refactoring

--- src/tiles.js
+++ src/tiles.js
@@ -1,20 +1,56 @@
-const g_ids = Object.freeze({
-    stone: 0,
-    dirt: 1,
-    grass: 2
-});
+import { game } from "./main.js";
 
 export function initTiles() {
-    const indices = [];
+    const ids = Object.freeze({
+        stone: 0,
+        dirt: 1,
+        grass: 2
+    });
 
-    indices[g_ids.stone] = 1;
-    indices[g_ids.dirt] = 4;
-    indices[g_ids.grass] = 0;
+    const numberOfIds = Object.keys(ids).length;
+
+    const indices = [];
+    const names = [];
+
+    indices[ids.stone] = 1;
+    indices[ids.dirt] = 4;
+    indices[ids.grass] = 0;
+    names[ids.stone] = "stone";
+    names[ids.dirt] = "dirt";
+    names[ids.grass] = "grass";
+
+    for (let i = 0; i < numberOfIds; ++i)  {
+        if (indices[i] == null) console.error(`no index for id ${i}`);
+        if (names[i] == null) console.error(`no name for id ${i}`);
+    }
 
     return {
-        ids: g_ids,
-        numberOfIds: Object.keys(g_ids).length,
-        indices: indices
+        getIndex,
+        getName,
+        getIdsFromIndex,
+        getIdsFromName,
+
+        ids,
+        numberOfIds,
+        indices
     };
 }
+
+function getIndex(id) {
+    return game.tiles.indices[id];
+}
+
+function getName(id) {
+    return game.tiles.name[id];
+}
+
+function getIdsFromIndex(index) {
+
+}
+
+function getIdsFromName(name) {
+
+}
+
+
 
